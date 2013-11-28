25317d29bbc10911fcc1c9c87b52cddead4f3e1f
cemoreno@Gmail.com <cemoreno@Gmail.com>
2013-11-28 20:21:52 +0100

    HAAAAAAAAAAND

---
+++ src/com/pezbailarin/ccest/UnitDetails.java
@@ -0,0 +1,20 @@
+package com.pezbailarin.ccest;
+
+public class UnitDetails
+{ 
+	private int icon;
+	private String nombre;
+
+	public void setIcon(int i){
+		icon=i;
+	}
+	public void setNombre(String n){
+		nombre=n;
+	}
+	public int getIcon(){
+		return icon;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+}
