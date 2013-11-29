082feec251868bb5abafd4f7668cfa66f76c8a8e
pezbailarin <pezbailarin@gmail.com>
2013-11-29 23:35:23 +0100

    por fin
    mi customListView adapter basado en un arrayList

---
+++ src/com/pezbailarin/ccest/ListCustomAdapter.java
@@ -0,0 +1,50 @@
+package com.pezbailarin.ccest;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListCustomAdapter extends ArrayAdapter<UnitDetails> {
+	 
+    Context context;
+ 
+    public ListCustomAdapter(Context context, int resourceId,
+            List<UnitDetails> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+ 
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        UnitDetails rowItem = getItem(position);
+ 
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.textUnidad);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.iconoUnidad);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+
+        holder.txtTitle.setText(rowItem.getNombre());
+        holder.imageView.setImageResource(rowItem.getIcon());
+ 
+        return convertView;
+    }
+}
