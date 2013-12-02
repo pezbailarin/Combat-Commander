fd52cd362d87187a41f11eafe8fceb5b20ab1899
pezbailarin <pezbailarin@gmail.com>
2013-12-03 00:06:37 +0100

    ImageFlip
    Funciona y gira las fichas

--- src/com/pezbailarin/ccest/MiDialogoActivity.java
+++ src/com/pezbailarin/ccest/MiDialogoActivity.java
@@ -8,22 +8,25 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import fr.castorflex.android.flipimageview.library.FlipImageView;
 
-public class MiDialogoActivity extends Activity {
+public class MiDialogoActivity extends Activity implements FlipImageView.OnFlipListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.dialog_custom);
+		setContentView(R.layout.dialog_custom2);
 
 		Intent intent = getIntent();
 		String message = intent.getStringExtra("NOMBRE");
 		TextView txt=(TextView)findViewById(R.id.txtNombreDialogo);
 		txt.setText(message);
 		
-		ImageView icono=(ImageView)findViewById(R.id.ivIconoDialogo);
-		icono.setImageResource(intent.getIntExtra("ICONO", R.drawable.nada));
+		FlipImageView  imagen=(FlipImageView) findViewById(R.id.ivIconoDialogo);
+		imagen.setDrawable(getResources().getDrawable(intent.getIntExtra("ICONO", R.drawable.nada)));
+		
+	//	ImageView icono=(ImageView)findViewById(R.id.ivIconoDialogo);
+	//	icono.setImageResource(intent.getIntExtra("ICONO", R.drawable.nada));
 		
 		txt=(TextView)findViewById(R.id.txtExtraDialogo);
 		txt.setText(intent.getStringExtra("EXTRAS"));
@@ -37,14 +40,31 @@ public class MiDialogoActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				finish();				
+				finish();	
 			}
 		});
 		
+		
+		
+		
 	}
 
+	@Override
+	public void onClick(FlipImageView view) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void onFlipStart(FlipImageView view) {
+		// TODO Auto-generated method stub
+		
+	}
 
-
+	@Override
+	public void onFlipEnd(FlipImageView view) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
