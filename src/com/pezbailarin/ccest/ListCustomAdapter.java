0f7c224c7cc025ec3becb8899994bca9b61925fc
pezbailarin <pezbailarin@gmail.com>
2013-12-12 23:17:00 +0100

    Muestra coste
    Ahora muestra el coste de cada unidad en el listview de la actividad
    principal, no solo en la ventana de detalle.

--- src/com/pezbailarin/ccest/ListCustomAdapter.java
+++ src/com/pezbailarin/ccest/ListCustomAdapter.java
@@ -26,12 +26,13 @@ public class ListCustomAdapter extends ArrayAdapter<UnitDetails> {
         ImageView imageView;
         TextView txtTitle;
         TextView txtExtra;
+        TextView txtCoste;
     }
  
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
-        UnitDetails rowItem = getItem(position);
- 
+        UnitDetails rowItem = getItem(position); 
+  
         LayoutInflater mInflater = (LayoutInflater) context
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         if (convertView == null) {
@@ -39,6 +40,7 @@ public class ListCustomAdapter extends ArrayAdapter<UnitDetails> {
             holder = new ViewHolder();
             holder.txtExtra = (TextView) convertView.findViewById(R.id.textUExtra);
             holder.txtTitle = (TextView) convertView.findViewById(R.id.textUnidad);
+            holder.txtCoste = (TextView) convertView.findViewById(R.id.txtCosteList);
             holder.imageView = (ImageView) convertView.findViewById(R.id.iconoUnidad);
             convertView.setTag(holder);
         } else
@@ -46,6 +48,7 @@ public class ListCustomAdapter extends ArrayAdapter<UnitDetails> {
 
         holder.txtTitle.setText(rowItem.getNombre());
         holder.txtExtra.setText(rowItem.getExtra());
+        holder.txtCoste.setText(String.valueOf(rowItem.getCoste()));
         holder.imageView.setImageResource(rowItem.getIcon());
  
         return convertView;
